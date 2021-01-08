45ee8ae61e605f386e676d4cf07a9fc62a3d6234
Ben Harvie <benharvie94@gmail.com>
2021-01-08 16:17:09 +0000

    renamed event env

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 // const core = require("@actions/core");
 
-console.log("Issue opened, event received:", process.env.EVENT_OUTPUT);
+console.log("Issue opened, event received:", process.env.EVENT);
